c5cfcaf2a2400880bc0010b9557855f208d0c0bf
John <john.whelan32@gmail.com>
2014-11-11 15:51:45 -0500

    Initial commit with contributors

---
+++ src/com/umassd/misc.java
@@ -0,0 +1,8 @@
+package com.umassd;
+
+
+private static void misc(String methodArgs) {
+		// TODO Auto-generated method stub
+		System.out.println("Method print "+ methodArgs);
+	}
+
---
+++ src/com/umassd/simulator.java
@@ -0,0 +1,21 @@
+package com.umassd;
+
+import java.util.*;
+
+
+public class simulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mainArgs;
+		mainArgs = "Passed";
+		misc(mainArgs);
+		System.out.println("Hello World");
+		
+	}
+}
+
+
